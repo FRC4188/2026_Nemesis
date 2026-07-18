d95078a25de68bd7c88a318a8e61e3c3c4766770
Priyanshu Biswal <priy.biswal@gmail.com>
2026-07-17 21:29:20 -0400

    sim fixes

--- src/main/java/frc/robot/subsystems/hood/HoodIOSim.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIOSim.java
@@ -1,42 +1,101 @@
 package frc.robot.subsystems.hood;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Constants;
 
+/** Lightweight hood simulation with position motion and electrical telemetry. */
 public class HoodIOSim implements HoodIO {
-  private double appliedVolts = 0;
-  private Rotation2d rotation = Rotation2d.kZero;
-  // private static final DCMotor HOOD_GEARBOX = DCMotor.getKrakenX60Foc(1);
-
-  //  private final DCMotorSim hoodSim;
-
-  // arbitrary values
-  // private static final double HOOD_KP = 0.8;
-  // private static final double HOOD_KD = 0.0;
-  // private PIDController hoodController = new PIDController(HOOD_KP, 0, HOOD_KD);
-
-  public HoodIOSim() {
-    // hoodSim =
-    //     new DCMotorSim(
-    //         LinearSystemId.createDCMotorSystem(
-    //             HOOD_GEARBOX, 0.004, Constants.HoodConstants.kGearRatio),
-    //         HOOD_GEARBOX);
+  private enum ControlMode {
+    VOLTAGE,
+    CURRENT,
+    POSITION
   }
+
+  private static final double LOOP_PERIOD_SECONDS = Constants.Robot.loopPeriodSecs;
+  private static final double MAX_SPEED_DEGREES_PER_SECOND = 180.0;
+  private static final double POSITION_KP_VOLTS_PER_DEGREE = 0.45;
+
+  private ControlMode controlMode = ControlMode.VOLTAGE;
+  private double commandedVolts = 0.0;
+  private double commandedCurrentAmps = 0.0;
+  private Rotation2d position = Rotation2d.kZero;
+  private Rotation2d targetPosition = Rotation2d.kZero;
+  private double appliedVolts = 0.0;
+  private double currentAmps = 0.0;
 
   @Override
   public void updateInputs(HoodIOInputs inputs) {
+    appliedVolts = calculateAppliedVolts();
+
+    double desiredSpeedDegreesPerSecond =
+        appliedVolts / 12.0 * MAX_SPEED_DEGREES_PER_SECOND;
+    double positionDeltaDegrees = desiredSpeedDegreesPerSecond * LOOP_PERIOD_SECONDS;
+
+    if (controlMode == ControlMode.POSITION) {
+      double errorDegrees = targetPosition.minus(position).getDegrees();
+      positionDeltaDegrees =
+          Math.copySign(
+              Math.min(Math.abs(positionDeltaDegrees), Math.abs(errorDegrees)), errorDegrees);
+    }
+
+    double nextPositionDegrees =
+        MathUtil.clamp(
+            position.getDegrees() + positionDeltaDegrees,
+            Constants.HoodConstants.Min_A.getDegrees(),
+            Constants.HoodConstants.Max_A.getDegrees());
+    position = Rotation2d.fromDegrees(nextPositionDegrees);
+
+    currentAmps =
+        controlMode == ControlMode.CURRENT
+            ? Math.abs(commandedCurrentAmps)
+            : Math.min(
+                Constants.HoodConstants.kStatorCurrent,
+                Math.abs(appliedVolts) / 12.0 * Constants.HoodConstants.kStatorCurrent);
+
+    inputs.motorConnected = true;
     inputs.appliedVolts = appliedVolts;
-    inputs.position = rotation;
+    inputs.currentAmps = currentAmps;
+    inputs.position = position;
   }
 
   @Override
   public void setVolts(double output) {
-    appliedVolts = output;
-    rotation = Rotation2d.kCCW_90deg;
+    controlMode = ControlMode.VOLTAGE;
+    commandedVolts = MathUtil.clamp(output, -12.0, 12.0);
   }
 
   @Override
-  public void setPosition(Rotation2d position) {
-    this.rotation = position;
-    appliedVolts = 0.0;
+  public void setCurrent(double output) {
+    controlMode = ControlMode.CURRENT;
+    commandedCurrentAmps =
+        MathUtil.clamp(
+            output,
+            Constants.HoodConstants.kPeakReverseTC,
+            Constants.HoodConstants.kPeakForwardTC);
+  }
+
+  @Override
+  public void setPosition(Rotation2d targetPosition) {
+    controlMode = ControlMode.POSITION;
+    this.targetPosition = targetPosition;
+  }
+
+  @Override
+  public void setZero() {
+    position = Rotation2d.kZero;
+    targetPosition = Rotation2d.kZero;
+  }
+
+  private double calculateAppliedVolts() {
+    return switch (controlMode) {
+      case VOLTAGE -> commandedVolts;
+      case CURRENT -> MathUtil.clamp(
+          commandedCurrentAmps / Constants.HoodConstants.kStatorCurrent * 12.0, -12.0, 12.0);
+      case POSITION -> MathUtil.clamp(
+          targetPosition.minus(position).getDegrees() * POSITION_KP_VOLTS_PER_DEGREE,
+          -12.0,
+          12.0);
+    };
   }
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -1,37 +1,106 @@
 package frc.robot.subsystems.shooter;
 
-public class ShooterIOSim implements ShooterIO {
+import edu.wpi.first.math.MathUtil;
+import frc.robot.Constants;
 
-  // private double appliedVolts = 0.0;
-  private double leftAppliedVolts = 0.0;
-  private double rightAppliedVolts = 0.0;
-  // private double RPM = 0.0;
+/** Lightweight shooter simulation with closed-loop velocity and electrical telemetry. */
+public class ShooterIOSim implements ShooterIO {
+  private enum ControlMode {
+    VOLTAGE,
+    TORQUE_CURRENT,
+    VELOCITY
+  }
+
+  private static final double LOOP_PERIOD_SECONDS = Constants.Robot.loopPeriodSecs;
+  private static final double RESPONSE_TIME_SECONDS = 0.18;
+  private static final double VELOCITY_KP_VOLTS_PER_RPM = 0.004;
+
+  private ControlMode controlMode = ControlMode.TORQUE_CURRENT;
+  private double commandedVolts = 0.0;
+  private double commandedCurrentAmps = 0.0;
+  private double targetRPM = 0.0;
   private double leftRPM = 0.0;
   private double rightRPM = 0.0;
-
-  public ShooterIOSim() {}
+  private double appliedVolts = 0.0;
+  private double currentAmps = 0.0;
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
-    // inputs.leftVelocityRPM = leftRPM;
-    // inputs.rightVelocityRPM = rightRPM;
+    appliedVolts = calculateAppliedVolts();
 
-    // inputs.leftAppliedVolts = leftAppliedVolts;
-    // inputs.rightAppliedVolts = rightAppliedVolts;
+    double freeSpeedRPM =
+        appliedVolts / 12.0 * Constants.ShooterConstants.kMaxRPM;
+    double responseFraction = 1.0 - Math.exp(-LOOP_PERIOD_SECONDS / RESPONSE_TIME_SECONDS);
+    leftRPM += (freeSpeedRPM - leftRPM) * responseFraction;
+    rightRPM += (freeSpeedRPM - rightRPM) * responseFraction;
+
+    double averageRPM = (leftRPM + rightRPM) / 2.0;
+    double backEmfVolts =
+        averageRPM / Constants.ShooterConstants.kMaxRPM * 12.0;
+    currentAmps =
+        controlMode == ControlMode.TORQUE_CURRENT
+            ? Math.abs(commandedCurrentAmps)
+            : Math.min(
+                Constants.ShooterConstants.kStatorCurrent,
+                Math.abs(appliedVolts - backEmfVolts)
+                    / 12.0
+                    * Constants.ShooterConstants.kStatorCurrent);
+
+    inputs.leftConnected = true;
+    inputs.rightConnected = true;
+    inputs.left2Connected = true;
+    inputs.left3Connected = true;
+
+    inputs.leftAppliedVolts = appliedVolts;
+    inputs.rightAppliedVolts = appliedVolts;
+    inputs.left2AppliedVolts = appliedVolts;
+    inputs.left3AppliedVolts = appliedVolts;
+
+    inputs.leftCurrentAmps = currentAmps;
+    inputs.rightCurrentAmps = currentAmps;
+    inputs.left2CurrentAmps = currentAmps;
+    inputs.left3CurrentAmps = currentAmps;
+
+    inputs.leftVelocityRPM = leftRPM;
+    inputs.rightVelocityRPM = rightRPM;
+    inputs.left2VelocityRPM = leftRPM;
+    inputs.left3VelocityRPM = leftRPM;
   }
 
   @Override
   public void setVolts(double volts) {
-    leftRPM = -100;
-    rightRPM = -100;
-    leftAppliedVolts = volts;
-    rightAppliedVolts = volts;
+    controlMode = ControlMode.VOLTAGE;
+    commandedVolts = MathUtil.clamp(volts, -12.0, 12.0);
+  }
+
+  @Override
+  public void setTorqueCurrent(double amps) {
+    controlMode = ControlMode.TORQUE_CURRENT;
+    commandedCurrentAmps =
+        MathUtil.clamp(
+            amps,
+            Constants.ShooterConstants.kPeakReverseTC,
+            Constants.ShooterConstants.kPeakForwardTC);
   }
 
   @Override
   public void setVelocity(double rpm) {
-    leftAppliedVolts = 0.0;
-    rightAppliedVolts = 0.0;
-    leftRPM = rpm;
+    controlMode = ControlMode.VELOCITY;
+    targetRPM = MathUtil.clamp(rpm, 0.0, Constants.ShooterConstants.kMaxRPM);
+  }
+
+  private double calculateAppliedVolts() {
+    return switch (controlMode) {
+      case VOLTAGE -> commandedVolts;
+      case TORQUE_CURRENT -> MathUtil.clamp(
+          commandedCurrentAmps / Constants.ShooterConstants.kStatorCurrent * 12.0, -12.0, 12.0);
+      case VELOCITY -> {
+        double averageRPM = (leftRPM + rightRPM) / 2.0;
+        double feedforwardVolts =
+            targetRPM / Constants.ShooterConstants.kMaxRPM * 12.0;
+        double feedbackVolts = (targetRPM - averageRPM) * VELOCITY_KP_VOLTS_PER_RPM;
+        yield MathUtil.clamp(feedforwardVolts + feedbackVolts, -12.0, 12.0);
+      }
+    };
   }
 }
